40b7f01173908d99f30b35a615406b9295527863
jennifxz <jenniferoliveira362@gmail.com>
2024-03-26 14:57:00 -0300

    endpoints

---
+++ api/src/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import usuarioController from './controller/usuarioController.js';
+
+
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+//configuração dos endpoints
+server.use(usuarioController);
+
+
+server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`));   
